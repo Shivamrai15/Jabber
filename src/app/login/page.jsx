91177d6d15187061ed6c5d0fc143858284cd7962
Shivam <shivamrai6836@gmail.com>
2023-12-05 01:32:39 +0530

    styling login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -24,31 +24,31 @@ const Login = () => {
     }
 
     return (
-        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-[url('/images/bg.jpg')] bg-cover">
-            <div className="w-full flex flex-col items-center justify-center max-w-md gap-y-8 bg-neutral-900 rounded-lg">
+        <div className="bg-neutral-900 flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="w-full flex flex-col items-center justify-center max-w-md gap-y-8 rounded-lg">
                 <div className="flex flex-col items-center gap-8 mt-10">
                     <Image 
                         src="/images/logo-dark.png"
                         alt="Logo"
-                        height={50}
-                        width={50}
+                        height={100}
+                        width={100}
                     />
-                    <h2 className="text-zinc-300 text-center text-sm font-semibold">
-                        Signin to you account
+                    <h2 className="text-zinc-300 text-center text-xl font-semibold">
+                        Sign in to you account
                     </h2>
                 </div>
-                <button
-                    className = "mb-10 w-72 h-16 border-[1px] text-white border-zinc-500 rounded-lg flex items-center justify-between px-4 cursor-default md:cursor-pointer"
-                    type="button"
-                    disabled = {isloading}
-                    onClick={loginWithGithub}
-                >
-                    <BsGithub className="w-10 h-10"/>
-                    <span>
-                        Github Account
-                    </span>
-                    <ChevronRight/>
-                </button>
+                    <button
+                        className = "w-72 h-16 border-[1px] text-white border-zinc-500 rounded-lg flex items-center justify-between px-4 cursor-default md:cursor-pointer hover:bg-neutral-800"
+                        type="button"
+                        disabled = {isloading}
+                        onClick={loginWithGithub}
+                    >
+                        <BsGithub className="w-10 h-10"/>
+                        <span>
+                            Github Account
+                        </span>
+                        <ChevronRight/>
+                    </button>
             </div>
         </div>
     );
